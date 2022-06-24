51ecaa17f6dc767a9df077c99a5ec05a38036b5c
Roman Grin <grinromchik@gmail.com>
2022-06-24 12:21:49 +0300

    commit: исправлена ошибка  таймером

--- second_win.py
+++ second_win.py
@@ -104,14 +104,18 @@ class TestWin(QWidget):
         self.timer.start(1000)
 
     def timer_sits(self):
-        global timer
-        time = QTime(0, 0, 45)
+        global time
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
         self.timer.timeout.connect(self.timer2Event)
+        # одно приседание в 1.5 секунды
         self.timer.start(1500)
+
+     # def timer_after_sits
 
     def timer_final(self):
         global time
-        time = QTime(0, 0, 45)
+        time = QTime(0, 1, 0)
         self.timer = QTimer()
         self.timer.timeout.connect(self.timer3Event)
         self.timer.start(1000)
@@ -121,7 +125,7 @@ class TestWin(QWidget):
         time = time.addSecs(-1)
         self.text_timer.setText(time.toString("hh:mm:ss"))
         self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
-        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setStyleSheet("color: rgb(111, 255, 0)")
         if time.toString("hh:mm:ss") == "00:00:00":
             self.timer.stop()
 
@@ -129,8 +133,8 @@ class TestWin(QWidget):
         global time
         time = time.addSecs(-1)
         self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
-        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
         self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
         if time.toString("hh:mm:ss") == "00:00:00":
             self.timer.stop()
 
@@ -138,12 +142,13 @@ class TestWin(QWidget):
         global time
         time = time.addSecs(-1)
         self.text_timer.setText(time.toString("hh:mm:ss"))
-        if (int(time.toString("hh:mm:ss")[6:8]) >= 40):
-            self.text_timer.setStyleSheet("color: rgb(0,0,0)")
-        elif (int(time.toString("hh:mm:ss")[6:8]) < 40) and (int(time.toString("hh:mm:ss")[6:8]) > 30):
-            self.text_timer.setStyleSheet("color: rgb(32, 248, 5)")
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
         else:
             self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
         if time.toString("hh:mm:ss") == "00:00:00":
             self.timer.stop()
 
